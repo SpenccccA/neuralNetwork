d91281d3ce35ef1d01600e3a64ce400cba247906
SpenccccA <spencertwagner@gmail.com>
2024-04-12 03:31:03 -0700

    Added Matrix Multiplication Function and moved all math functions to matrixMath.h

--- src/main.cpp
+++ src/main.cpp
@@ -1,61 +1,13 @@
 #include <iostream>
 #include <vector>
+#include <matrixMath.h>
 
-
-// May optimize if possible at a later date ~
-// Will also add to include math file later when all math functions are finished
-float dotp(const std::vector<float>& vec1, const std::vector<float>& vec2) {
-
-	if (vec1.size() != vec2.size()) 
-	{ 
-		std::cout << "Cannot dot product different size vectors" << std::endl; 
-		throw std::runtime_error("Error"); 
-	}
-
-	float result{0};
-
-	for (unsigned int i{0}; i < vec1.size(); i++)
-	{
-
-		result += (vec1.at(i) * vec2.at(i));
-
-	}
-
-	return result;
-}
-
-//Switch the columns and rows of a 2d matrix
-std::vector<std::vector<float>> transpose(const std::vector<std::vector<float>>& M)
-{
-	if (M.size() == 0)
-	{
-		std::cout << "Invalid matrix size, cannot transpose" << std::endl;
-		throw std::runtime_error{ "Transpose Error" };
-	}
-
-
-	std::vector<std::vector<float>> result{ };
-
-	for (unsigned int i{ }; i < M.at(0).size(); i++)
-	{
-		std::vector<float> tempVector{ };
-
-		for (unsigned int j{ }; j < M.size(); j++)
-		{
-			tempVector.push_back(M.at(j).at(i));
-		}
-		result.push_back(tempVector);
-	}
-
-	return result;
-
-}
 
 int main() {
 	// Test data, will either be input data into input layer, or output data from other neurons later
-	std::vector<std::vector<float>> inputs{ 
-		{1.0f, 2.0f, 3.0f, 2.5f }, 
-		{ 2.0f, 5.0f, -1.0f, 2.0f }, 
+	std::vector<std::vector<float>> inputs{
+		{1.0f, 2.0f, 3.0f, 2.5f },
+		{ 2.0f, 5.0f, -1.0f, 2.0f },
 		{ -1.5f, 2.7f, 3.3f, -0.8f }
 	};
 
@@ -73,8 +25,25 @@ int main() {
 		2.0f, 3.0f, 0.5f
 	};
 
-
+	// Unusued at the moment, will be used in the future.
 	std::vector<float> outputs{ };
+
+	// Code below can be deleted at any time. Code for testing if matricies are correct.
+	/*
+	std::vector<std::vector<float>> temp { matrixMult(inputs, weights) };
+	for (unsigned int i{ }; i < temp.size(); i++)
+	{
+		for (unsigned int j{ }; j < temp.at(0).size(); j++)
+		{
+
+			std::cout << temp.at(i).at(j) + biases.at(j) << " ";
+
+		}
+
+		std::cout << std::endl;
+	}
+	*/
+
 
 
 	return 0;
